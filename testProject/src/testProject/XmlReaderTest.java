a0ae73c5d37297b325a5e412cb6a832d0291a2d4
Ryota.Iwasaki <riwasaki0606@gmail.com>
2021-04-16 00:20:18 +0900

    xmlを読み込むためのテストを行った

---
+++ testProject/src/testProject/XmlReaderTest.java
@@ -0,0 +1,56 @@
+package testProject;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Paths;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+public class XmlReaderTest {
+
+	public static void main(String[] args) {
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		try {
+			FileInputStream is = new FileInputStream(Paths.get("C:/temp/TankConfig.xml").toFile());
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			Document document = builder.parse(is);
+			XPath xPath = XPathFactory.newInstance().newXPath();
+
+			XPathExpression expression = xPath.compile("TankBattleConfig/Tank");
+
+			NodeList nList = (NodeList) expression.evaluate(document, XPathConstants.NODESET);
+
+
+//			System.out.println("nodeName:" + element.getNodeName());
+//			System.out.println("nodeValue:" + element.getNodeValue());
+
+//			NodeList list = element.getChildNodes();
+
+			for(int n = 0; n < nList.getLength(); n++) {
+				Element element2 = (Element)nList.item(n);
+
+				System.out.println((n+1) + "台目");
+				System.out.println("name:" + element2.getAttribute("name"));
+				System.out.println("攻撃性:" + element2.getAttribute("offensive"));
+			}
+
+		} catch (ParserConfigurationException | IOException | SAXException | XPathExpressionException e ) {
+			// TODO 自動生成された catch ブロック
+			System.out.println("エラーが発生");
+			e.printStackTrace();
+		}
+	}
+
+}
